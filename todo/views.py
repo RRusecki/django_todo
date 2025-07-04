03215894bb618a435db3d9ad96f131d885513f8c
RRusecki <robertr19941@gmail.com>
2025-07-04 13:37:35 +0300

    Prideta autentifikacija

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Task
+from django.contrib.auth.decorators import login_required
+from . import views
 
-# Create your views here.
+@login_required
+def task_list(request):
+    task = Task.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'todo/task_list.html', {'task': tasks})
